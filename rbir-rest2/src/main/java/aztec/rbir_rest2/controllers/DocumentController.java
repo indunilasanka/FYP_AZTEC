619094d326d8112ecbbdf1a619f492846380772d
Kelum Bandara <kelum.13@cse.mrt.ac.lk>
2017-05-28 16:38:51 +0530

    Added rest call for file uploading

--- rbir-rest2/src/main/java/aztec/rbir_rest2/controllers/DocumentController.java
+++ rbir-rest2/src/main/java/aztec/rbir_rest2/controllers/DocumentController.java
@@ -1,5 +1,8 @@
 package aztec.rbir_rest2.controllers;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
@@ -8,7 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import aztec.rbir_database.Entities.Document;
 import aztec.rbir_database.Entities.Keyword;
@@ -50,5 +56,24 @@ public class DocumentController {
         data.insert(doc, keywords);
         
         return new ResponseEntity("Product saved successfully", HttpStatus.OK);
+    }
+    
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public @ResponseBody String handleFileUpload( 
+            @RequestParam("file") MultipartFile file){
+            String name = "test11";
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
     }
 }
