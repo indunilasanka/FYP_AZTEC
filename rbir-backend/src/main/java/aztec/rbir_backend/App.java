38fdcb37d87da39b79082c7dad3f113d01432948
Subhash <subhashrodrigo7@gmail.com>
2017-05-29 11:35:42 +0530

    Add new libraries, Change the backend with facilitating elastic search

--- rbir-backend/src/main/java/aztec/rbir_backend/App.java
+++ rbir-backend/src/main/java/aztec/rbir_backend/App.java
@@ -1,5 +1,20 @@
 package aztec.rbir_backend;
 
+import aztec.rbir_backend.configurations.ElasticSearchClient;
+import aztec.rbir_backend.logic.DocumentSeeker;
+import aztec.rbir_backend.logic.FileReaderAndIndexer;
+import aztec.rbir_backend.logic.FileReaderFactory;
+import aztec.rbir_backend.logic.MyFileReader;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
 /**
  * Hello world!
  *
@@ -8,6 +23,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//           FileReaderAndIndexer fri = new FileReaderAndIndexer();
+//           fri.indexDocument("aztec","E://Exercise-03.docx");
+        DocumentSeeker seeker = new DocumentSeeker();
+        String[] keys = {"ns",
+                "time"};
+        seeker.searchDocument("aztec",keys);
     }
 }
