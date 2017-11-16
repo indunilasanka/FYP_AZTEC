02f148b052481ac3b2296a72008e5d2c120f83b6
ekanayakamsl <sarangaekanayaka.13@cse.mrt.c.lk>
2017-11-16 11:00:33 +0530

    change password

--- rbir-frontend/src/app/pages/changePassward/changePassward.component.ts
+++ rbir-frontend/src/app/pages/changePassward/changePassward.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { EmailValidator, EqualPasswordsValidator } from '../../theme/validators';
 
 @Component({
   selector: 'change_passward',
@@ -10,15 +12,26 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class ChangePassward implements OnInit {
 
   modalHeader: string;
-  modalContent: string = `Lorem ipsum dolor sit amet,
-   consectetuer adipiscing elit, sed diam nonummy
-   nibh euismod tincidunt ut laoreet dolore magna aliquam
-   erat volutpat. Ut wisi enim ad minim veniam, quis
-   nostrud exerci tation ullamcorper suscipit lobortis
-   nisl ut aliquip ex ea commodo consequat.`;
+  modalContent: string;
+  public form: FormGroup;
+  public submitted: boolean = false;
+  public password: AbstractControl;
+  public repeatPassword: AbstractControl;
+  public passwords: FormGroup;
 
-  constructor(private activeModal: NgbActiveModal) {
+  constructor(private activeModal: NgbActiveModal, fb: FormBuilder) {
+    this.form = fb.group({
+      'passwords': fb.group({
+        'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+        'repeatPassword': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+      }, { validator: EqualPasswordsValidator.validate('password', 'repeatPassword')})
+    });
+    this.passwords = <FormGroup> this.form.controls['passwords'];
+    this.password = this.passwords.controls['password'];
+    this.repeatPassword = this.passwords.controls['repeatPassword'];
+
   }
+
 
   ngOnInit() {}
 
